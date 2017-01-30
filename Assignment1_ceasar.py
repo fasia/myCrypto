2c0935daa14e7610300b242fb973ae910d4596d8
Faezeh Siavashi <fasiava@gmail.com>
2017-01-30 14:22:46 +0200

    Update Assignment1_ceasar.py

--- Assignment1_ceasar.py
+++ Assignment1_ceasar.py
@@ -1,25 +1,32 @@
-# Caesar Cipher
+# Author: Faezeh Siavashi
+# Date: Jan 23 2017
+#***********************************
+# Method: Caesar
+# Alphabet: ASCII table, all of 256 symbols
+# Data Type: Some Microsoft Document Text Format
 
 MAX_KEY_SIZE = 256
 
-
+#read the encoded file
 def getFile():
     print('give encoded file name:')
-    return open('file1-35121.rtf.enc').read()
+    return open('file1-35121.rtf.enc').read() # this file can be found in myCrypto Folder
 
-
+#decode the file based on current key
 def getTranslatedMessage(message):
         translated = ''
         for symbol in message:
-            num = (ord(symbol)-(currentKey))%256
+            num = (ord(symbol)-(currentKey))%MAX_KEY_SIZE # caeser decoding folmula: decode=(encode-key)mod MAX_KEY_SIZE
             translated += chr(num)
         return translated
 
 
+    
 encoded_file = getFile()
 currentKey =1
-text_file = open("de.txt", "a")
 
+#output file
+text_file = open("de.txt", "a")
 while (currentKey<MAX_KEY_SIZE):
     text_file.write(getTranslatedMessage(encoded_file))
     currentKey +=1
